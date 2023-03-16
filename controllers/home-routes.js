0e4b5dc1b9cab427ea213424dbe8f4941a2422f3
Valeriya <valeriya.finogeeva@gmail.com>
2023-03-16 14:29:17 +1100

    Fixing comment section

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,21 +29,44 @@ router.get('/login', (req, res) => {
 
 
 // get single post
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const postData = await Post.findByPk(req.params.id, {
+//             include: [
+//                 User,
+//                 {
+//                     model: Comment,
+//                     include: [User],
+//                 },
+//             ],
+//         });
+//         res.render('single-post', {
+//             layout: 'main',
+//             postData,
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
 router.get('/:id', async (req, res) => {
     try {
-        const postData = await Post.findByPk(req.params.id, {
-            include: [
-                User,
-                {
-                    model: Comment,
-                    include: [User],
-                },
-            ],
-        });
+        const postData = await Post.findByPk(req.params.id);
+        const commentData = await Comment.findAll({
+            where: {
+                id: req.params.id
+            },
+            include: [{model: User}],
+        })
+        const comments = commentData.map((post) => post.get({plain: true}));
+        const singlePost = postData.get({ plain: true})
         res.render('single-post', {
-            layout: 'main',
+            ...singlePost,
+            comments,
+            logged_in: req.session.logged_in
         });
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
     }
 });
